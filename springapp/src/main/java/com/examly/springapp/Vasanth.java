00ee7258456a35ca67c248b9834d2e4acff5be5c
neojarvis <techadmin@iamneo.ai>
2024-01-23 19:01:52 +0000

    Submit-project-save-commit-1/24/2024--12:31:49-AM

--- springapp/src/main/java/com/examly/springapp/Vasanth.java
+++ springapp/src/main/java/com/examly/springapp/Vasanth.java
@@ -3,5 +3,6 @@ package com.examly.springapp;
 public class Vasanth {
     public static void main(String[] args) {
         System.out.println("welcome");
+        System.out.println("submit project");
     }
 }
